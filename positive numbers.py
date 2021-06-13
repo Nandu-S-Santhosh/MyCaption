22120100a2c32dfc5640e4a4e92d2e36d5f4ad70
Nandu-S-Santhosh <85701438+Nandu-S-Santhosh@users.noreply.github.com>
2021-06-14 03:32:57 +0530

    Update positive numbers.py

--- positive numbers.py
+++ positive numbers.py
@@ -5,9 +5,9 @@
 list1 = [12, -7, 5, 64, -14]
 list2 = [12, 14, -95, 3]
 #merging each element of both lists
-list3=list1+list2
+list1.extend(list2)
 # iterating each number in list
-for num in list3:
+for num in list1:
     # checking condition
     if num >= 0:
        print(num, end = " ")
