60595791e54743bc358be3b4bb58ec69632a3d09
Nandu-S-Santhosh <85701438+Nandu-S-Santhosh@users.noreply.github.com>
2021-06-22 21:57:15 +0530

    Create decreasing order of frequency.py
    Write Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency. Use dictionaries.

---
+++ decreasing order of frequency.py
@@ -0,0 +1,14 @@
+import operator
+
+if __name__ == '__main__':
+
+    test_string = 'Mississippi'
+
+    string_dict = dict()
+    for letter in test_string:
+        if letter not in string_dict:
+            string_dict[letter] = test_string.count(letter)
+
+    # Sort dictionary by values
+ordered_answer = sorted(string_dict.items(), key=operator.itemgetter(1), reverse=True)
+print (ordered_answer)
